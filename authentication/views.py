373e33eb469b0155865219b76ce17c2be13a8021
George Hlongwane <georgehlongwane8@gmail.com>
2023-08-29 14:54:55 +0200

    Tried to add email verification

--- authentication/views.py
+++ authentication/views.py
@@ -2,15 +2,28 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from .models import Verification
+from helpers import send_mail
+from django.contrib.sites.models import Site
 
 
 # Create your views here.
 def register(request):
+
     if request.method == "POST":
         email = request.POST["email"]
         password = request.POST["password"]
 
         User.objects.create_user(email=email, username=email, password=password)
+        user = User.objects.get(username=email)
+
+        Verification(user_id=user.id).save()
+        v = Verification.objects.get(user_id=user.id)
+        domain = Site.objects.get_current().domain
+        if domain == "example.com":
+            domain = "127.0.0.1:8000"
+
+        send_mail(email, "Verification for login attempt", f"Please click this <a href='http://{domain}/auth/verify/{v.code}'>link</a> to verify your account") # used "get base url in Django" to get back to our application for registration 
 
         return render(request, "authentication/registered.html")
     else:
@@ -24,10 +37,19 @@ def login_view(request):
         user = authenticate(username=email, password=password)
 
         if user is not None:
+            try:
+                Verification.objects.get(user_id=user.id)
+                return render(request, "authentication/invalid.html", {
+                    "message": "Please complete your email verfication. Click <a href='/auth/verify/new'>here</a> to re-request verification"
+                })
+            except Verification.DoesNotExist:
+                login(request, user)
             login(request, user)
             return HttpResponseRedirect("/")
         else:
-            return render(request, "authentication/invalid.html") 
+            return render(request, "authentication/invalid.html", {
+                "message": "Invalid credentials"
+            })                 
     else:
         return render(request, "authentication/login.html")
 
@@ -35,3 +57,12 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect("/")
 
+def verify(request, code):
+    try:
+        Verification.objects.get(code=code).delete()
+        return HttpResponseRedirect("/auth/login/")
+    except Verification.DoesNotExist:
+        return render(request, "invalid.html", {
+            "message": "Code is not valid or has already been used"
+        })
+    
--- quickauth/settings.py
+++ quickauth/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # added application
+    'django.contrib.sites',
     # custom application
     'authentication',
 ]
@@ -123,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SITE_ID = 1
